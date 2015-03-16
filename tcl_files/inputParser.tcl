df1e7cbab75aebce60c4d22d9a736dc2f54b3f38
Kris <sydesyrius@gmail.com>
2015-03-16 23:54:45 +0100

    enh: dodano wykrywanie powtarzajacych sie nazw przy wczytywaniu pliku

--- tcl_files/inputParser.tcl
+++ tcl_files/inputParser.tcl
@@ -4,7 +4,7 @@ proc wczytaj_plik_wejsciowy {file_name} {
     puts "ERROR: wskazany plik '$file_name' nie istnieje."
     exit 1
   }
-  
+  global name_list
   # Open file
   set fh [open $file_name r]
   set file_content [read $fh]
@@ -13,13 +13,13 @@ proc wczytaj_plik_wejsciowy {file_name} {
     set single_line_no_space [string trim $single_line]
     if {[regexp {^[A-Za-z]+$} $single_line_no_space tmp parent]} {
       #puts "This is a parent name $parent"
-      addToClist $single_line_no_space
+      add_to_single_name $single_line_no_space
     } elseif {[regexp {^-[A-Za-z]+$} $single_line_no_space tmp first_child]} {
       #puts "This is a first child $first_child"
-      addToClist $single_line_no_space
+      add_to_single_name [string range $single_line_no_space 1 end]
     } elseif {[regexp {^--[A-Za-z]+$} $single_line_no_space tmp second_child]} {
       #puts "This is a first child $second_child"
-      addToClist $single_line_no_space
+      add_to_single_name [string range $single_line_no_space 2 end]
     } elseif {[regexp {^$} $single_line_no_space]} {
       #puts "Can be ignored" - empty line
     } else {
@@ -29,4 +29,16 @@ proc wczytaj_plik_wejsciowy {file_name} {
     }
   }
 
+}
+
+proc add_to_single_name {component_name} {
+  global name_list
+  regsub -nocase "\\-" $component_name "" component_no_minus
+  if {$component_no_minus ni $name_list(original)} {
+    append name_list(original) "$component_no_minus  "
+    addToClist $component_name
+  } else {
+    puts "ERROR: Nazwa została wcześniej użyta '$component_name'."
+    exit 1
+  }
 }
