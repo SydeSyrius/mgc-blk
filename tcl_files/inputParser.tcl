5c5ddbbbf989e3363067765229448eafcf5a7262
SydeSyrius <sydesyrius@gmail.com>
2015-04-13 22:32:58 +0200

    environmental variables added to insert defects

--- tcl_files/inputParser.tcl
+++ tcl_files/inputParser.tcl
@@ -1,16 +1,19 @@
 proc sourceFile {file_name} {
-	if { "TCL_OK" != [checkFile $file_name] } {
-		return TCL_ERROR
-	}
-	puts "Executing file: $file_name"
-	source $file_name
-	return TCL_OK
+  if { "TCL_OK" != [checkFile $file_name] } {
+    return TCL_ERROR
+  }
+  puts "Executing file: $file_name"
+  source $file_name
+  return TCL_OK
 }
 proc writeFileCheck {file_name} {
-	if {![file exist $file_name]} {
+  if {![file exist $file_name]} {
     return TCL_OK
   }
-	puts "\[ERROR\] File '$file_name' already exists.'"
+  if {[info exist ::env(env_td_out_file_append)]} {
+    return TCL_OK
+  }
+  puts "\[ERROR\] File '$file_name' already exists.'"
   return TCL_ERROR
 }
 
@@ -19,15 +22,15 @@ proc checkFile {file_name} {
     puts "\[ERROR\] File '$file_name' does not exist."
     return TCL_ERROR
   }
-	return TCL_OK	
+  return TCL_OK	
 }
 
 proc read_file {file_name} {
-	if { "TCL_OK" != [checkFile $file_name] } {
-		return TCL_ERROR
-	}
-	# First remove everything from memory
-	delete_object
+  if { "TCL_OK" != [checkFile $file_name] } {
+    return TCL_ERROR
+  }
+  # First remove everything from memory
+  delete_object
   global name_list
   # Open file
   set fh [open $file_name r]
@@ -37,29 +40,41 @@ proc read_file {file_name} {
     set single_line_no_space [string trim $single_line]
     if {[regexp {^[A-Za-z]+$} $single_line_no_space tmp parent]} {
       #puts "This is a parent name $parent"
-			set parent $single_line_no_space
+      set parent $single_line_no_space
       add_object $parent
-			if {[info exists child]} {
-				unset child 
-			}
+      if {[info exists child]} {
+        unset child 
+      }
     } elseif {[regexp {^-[A-Za-z]+$} $single_line_no_space tmp first_child]} {
       #puts "This is a first child $first_child"
-			regsub -nocase "\\-" $single_line_no_space "" child
-			if {![info exists parent]} {
-				puts "\[ERROR\] Wrong hierarchy."
-				delete_object
-				return TCL_ERROR
-			}
+      regsub -nocase "\\-" $single_line_no_space "" child
+      if {![info exists parent]} {
+        if {[info exist ::env(env_td_missing_error_msg)]} {
+          puts "ASSERTION CAUGHT!"
+          exit 1
+        }
+        puts "\[ERROR\] Wrong hierarchy: $single_line_no_space "
+        delete_object
+        return TCL_ERROR
+      }
       add_object $child -below $parent
-    } elseif {[regexp {^--[A-Za-z]+$} $single_line_no_space tmp second_child]} {
+    } elseif {![info exist ::env(env_td_wrong_hierarchy)] && [regexp {^--[A-Za-z]+$} $single_line_no_space tmp second_child]} {
       #puts "This is a first child $second_child"
-			regsub -nocase "\\--" $single_line_no_space "" childChild 
-			if {![info exists child]} {
-        puts "\[ERROR\] Wrong hierarchy."
-				delete_object
-				return TCL_ERROR
-			}
+      regsub -nocase "\\--" $single_line_no_space "" childChild 
+      if {![info exists child]} {
+        puts "\[ERROR\] Wrong hierarchy: $single_line_no_space"
+        delete_object
+        return TCL_ERROR
+      }
       add_object $childChild -below $parent/$child
+    } elseif {[info exist ::env(env_td_wrong_hierarchy)] && [regexp {^-[-]+[A-Za-z]+$} $single_line_no_space tmp second_child]} {
+      regsub -nocase -all "\\-" $single_line_no_space "" childChild 
+      if {![info exists child]} {
+        puts "\[ERROR\] Wrong hierarchy: $single_line_no_space"
+        delete_object
+        return TCL_ERROR
+      }
+      add_object $childChild -below $parent/$child  
     } elseif {[regexp {^$} $single_line_no_space]} {
       #puts "Can be ignored" - empty line
     } else {
@@ -71,11 +86,11 @@ proc read_file {file_name} {
 	return TCL_OK
 }
 proc validateCmdLine_addObject {component_name} {
-	if {![regexp {^\s*[A-Za-z]+\s*(\s+-below\s+[A-Za-z]+(/[A-Za-z]+){0,1})*\s*$} $component_name]} {
-		puts "\[ERROR\] Syntax error."
-	  return TCL_ERROR
-	}
-	return TCL_OK
+  if {![regexp {^\s*[A-Za-z]+\s*(\s+-below\s+[A-Za-z]+(/[A-Za-z]+){0,1})*\s*$} $component_name]} {
+    puts "\[ERROR\] Syntax error: $component_name"
+    return TCL_ERROR
+  }
+  return TCL_OK
 }
 
 proc validateCmdLine_deleteObject {component_name} {
@@ -87,16 +102,26 @@ proc validateCmdLine_deleteObject {component_name} {
 }
 
 proc validateCmdLine_displayObject {component_name} {
-	if {![regexp {^\s*(-below\s+[A-Za-z]+(/[A-Za-z]+){0,1})*\s*$} $component_name]} {
-		puts "\[ERROR\] Syntax error."
-		return TCL_ERROR
-	}
-	return TCL_OK
+  if {[info exist ::env(env_td_display)]} {
+    if {![regexp {^\s*(-below\s+[A-Za-z]+(/+[A-Za-z]+){0,1})*\s*$} $component_name]} {
+      puts "\[ERROR\] Syntax error."
+      return TCL_ERROR
+    }
+  } else {
+    if {![regexp {^\s*(-below\s+[A-Za-z]+(/[A-Za-z]+){0,1})*\s*$} $component_name]} {
+      puts "\[ERROR\] Syntax error."
+      return TCL_ERROR
+    }
+  }
+  return TCL_OK
 }
 proc add_to_single_name {component_name} {
   global name_list
   if {$component_name ni $name_list(original)} {
     append name_list(original) "$component_name  "
+    if {![info exist ::env(env_td_print_internal_data)]} {
+      puts $name_list(original)
+    }
     return TCL_OK
   } else {
     puts "\[ERROR\] Name '$component_name' already exists."
@@ -106,31 +131,31 @@ proc add_to_single_name {component_name} {
 
 proc removeFromSingle {component_name} {
   global name_list
-	foreach removeInstance $component_name {
-	  set index [lsearch -exact $name_list(original) $removeInstance]
-  		if { $index == -1 } {
-				puts "\[ERROR\] Object does not exist."
-				return TCL_ERROR
-  		} else {
-    		set name_list(original) [lreplace $name_list(original) $index $index]
-  		}
-	}
-	return TCL_OK
+  foreach removeInstance $component_name {
+    set index [lsearch -exact $name_list(original) $removeInstance]
+    if { $index == -1 } {
+      puts "\[ERROR\] Object does not exist."
+      return TCL_ERROR
+    } else {
+      set name_list(original) [lreplace $name_list(original) $index $index]
+    }
+  }
+  return TCL_OK
 }
 
 proc changeLetter {letter new_letter} {
   global name_list
   if { [regsub -all "$letter" $name_list(original) "$new_letter" name_list(tmp)]} {
-  	if { [llength [lsort $name_list(tmp)]] != [llength [lsort -unique $name_list(tmp)]] } {
-		puts "\[ERROR\] Duplicated name." 
-	  } else {
-		set $name_list(original) $name_list(tmp)
-	  }
+    if { [llength [lsort $name_list(tmp)]] != [llength [lsort -unique $name_list(tmp)]] } {
+      puts "\[ERROR\] Duplicated name." 
+    } else {
+      set $name_list(original) $name_list(tmp)
+    }
   } else {
-       puts "\[ERROR\] Cannot find this letter."
+    puts "\[ERROR\] Cannot find this letter."
   }
 }
 
 proc memoryMeasure {} {
-	puts "I used"
+  puts "I used"
 }
